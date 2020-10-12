7f9f41dcb43dfad63a39bbf6c2e36a30ab14e8f5
Mark Camilleri <markcamilleri10@gmail.com>
2020-10-12 13:32:38 -0400

    Xcode fix

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import "./styles/index.scss";
+import "./styles/index.css";
 import "./images/yoda-stitch.jpg";
 import canvasExample from "./scripts/canvas";
 import Square from "./scripts/square";
